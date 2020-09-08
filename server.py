329dbd9cee2ff8eb572864fc4f61ca999e297960
Maestro-Zacht <61846068+Maestro-Zacht@users.noreply.github.com>
2020-09-08 10:38:09 +0200

    prova heroku

--- server.py
+++ server.py
@@ -1,21 +1,35 @@
 import os
 
-from twisted.internet import protocol, reactor
+from twisted.internet import reactor
+from twisted.internet.endpoints import TCP4ServerEndpoint
+from twisted.internet.protocol import Protocol, ServerFactory
 
 PORT = int(os.environ.get('PORT', 12345))
 
 
-class Server(protocol.Protocol):
+class Server(Protocol):
+
+    def __init__(self, collegati):
+        self.collegati = collegati
+
     def dataReceived(self, data):
         self.transport.write(data)
 
+    def connectionMade(self):
+        print('New connection')
 
-class ServerFactory(protocol.ServerFactory):
+
+class ServerFactory(ServerFactory):
+
+    def __init__(self):
+        self.collegati = {}
+
     def buildProtocol(self, addr):
-        return Server()
+        return Server(self.collegati)
 
 
 if __name__ == '__main__':
     print(f'ascolto sulla porta {PORT}')
-    reactor.listenTCP(PORT, ServerFactory())
+    endpoint = TCP4ServerEndpoint(reactor, PORT)
+    endpoint.listen(ServerFactory())
     reactor.run()
